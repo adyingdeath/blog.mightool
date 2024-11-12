28bd49afda3c4a36a866c3221d46520b0c164c70
CatDevil <2975527435@qq.com>
2024-11-12 23:10:28 +0800

    first commit

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,10 @@
+const headerNavLinks = [
+  { href: 'https://www.mightool.com/en/', title: 'Mightool' },
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'Blog' },
+  { href: '/tags', title: 'Tags' },
+  { href: '/projects', title: 'Projects' },
+  { href: '/about', title: 'About' },
+]
+
+export default headerNavLinks
