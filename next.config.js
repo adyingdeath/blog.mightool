c9438c5c93269b16f91565e7f1604cdd0c4a0bff
CatDevil <2975527435@qq.com>
2025-01-31 16:38:15 +0800

    Add trailingSlash:true to create index.html instead of (pageName).html

--- next.config.js
+++ next.config.js
@@ -67,6 +67,7 @@ module.exports = () => {
     output,
     basePath,
     reactStrictMode: true,
+    trailingSlash: true,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
     eslint: {
       dirs: ['app', 'components', 'layouts', 'scripts'],
